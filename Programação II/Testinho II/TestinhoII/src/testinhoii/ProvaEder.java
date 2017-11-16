b105847c3004bb75c37d8d318a7cc2201b1040d3
Diego Armando Cacilha <cacilha.tec@gmail.com>
2017-11-16 21:46:55 -0200

    Aula de prog II - Início do trabalho final

---
+++ Programação II/Testinho II/TestinhoII/src/testinhoii/ProvaEder.java
@@ -0,0 +1,44 @@
+package provaeder;
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.Scanner;
+import javax.xml.crypto.Data;
+
+public class ProvaEder {
+
+    
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Date datAtu = new Date();
+        
+        System.out.print("informe o dia de nascimento: ");
+        String dia = teclado.next();
+        
+        System.out.print("informe o mês de nascimento: ");
+        String mes = teclado.next();
+        
+        System.out.print("informe o ano de nascimento: ");
+        String ano = teclado.next();
+        Date nascimento = new Date(mes+"/"+dia+"/"+ano);
+        
+        
+        
+        if (nascimento.getYear() > datAtu.getYear()) {
+            System.out.println("O ano é futuro");
+        } else if (nascimento.getYear() == datAtu.getYear() && nascimento.getMonth() > datAtu.getMonth()) {
+            System.out.println("O mês é futuro");
+        } else if (nascimento.getYear() == datAtu.getYear() && nascimento.getMonth() == datAtu.getMonth() && nascimento.getDate() > datAtu.getDate()) {
+            System.out.println("O dia é futuro");
+        }else if(nascimento.getDate() == datAtu.getDate() && nascimento.getMonth() == datAtu.getMonth()){
+            System.out.println("Parabéns! Hoje é seu " + (datAtu.getYear() - nascimento.getYear()) + "º aniversário.");
+        }else if(nascimento.getMonth() > datAtu.getMonth()){
+            System.out.println("Pessoa com " + (datAtu.getYear() - nascimento.getYear() - 1) + " anos.");
+        }else if (nascimento.getMonth() == datAtu.getMonth() && nascimento.getDate() > datAtu.getDate()){
+            System.out.println("Pessoa com " + (datAtu.getYear() - nascimento.getYear()-1) + " anos.");
+        }else if (nascimento.getMonth() <= datAtu.getMonth() && nascimento.getDate() < datAtu.getMonth())
+            System.out.println("Pessoa com " + (datAtu.getYear() - nascimento.getYear()) + " anos.");
+      
+    }
+    
+}
